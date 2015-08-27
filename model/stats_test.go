4ad236133aecade41ecec70b4f0036d27de8deb1
Léo Cavaillé <leo@datadoghq.com>
2015-08-27 14:24:27 -0400

    Fix error counts. Better tests for counts.

--- model/stats_test.go
+++ model/stats_test.go
@@ -6,75 +6,57 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCountHits(t *testing.T) {
+func TestCounts(t *testing.T) {
 	assert := assert.New(t)
 
 	tags := NewTagsFromString("version:34.42,resource:/dash/list,service:dogweb")
-	c := NewCount(HITS, tags)
+	testCounts := []Count{
+		NewCount(HITS, tags),
+		NewCount(ERRORS, tags),
+		NewCount(DURATION, tags),
+		NewCount("custom_size", tags),
+	}
 
-	// Our fake span
-	s := Span{TraceID: RandomID(), SpanID: RandomID(), Start: Now(), Duration: 1.0}
-	c = c.Add(s)
-
-	assert.Equal(HITS, c.Name)
-	assert.Equal(1, c.Value)
-	assert.Equal(
-		TagSet{
-			Tag{Name: "version", Value: "34.42"},
-			Tag{Name: "resource", Value: "/dash/list"},
-			Tag{Name: "service", Value: "dogweb"},
+	// Our fake spans
+	spans := []Span{
+		Span{Duration: 10},
+		Span{Error: 1, Duration: 25},
+		Span{
+			Duration: 15,
+			Metrics:  map[string]int64{"custom_size": 3},
 		},
-		c.TagSet,
-	)
-}
-
-func TestCountErrors(t *testing.T) {
-	assert := assert.New(t)
-
-	tags := NewTagsFromString("version:34.42,resource:/dash/list,service:dogweb")
-	c := NewCount(ERRORS, tags)
-
-	// Our fake span
-	s := Span{TraceID: RandomID(), SpanID: RandomID(), Start: Now(), Duration: 1.0}
-	c = c.Add(s)
-
-	assert.Equal(ERRORS, c.Name)
-	assert.Equal(1, c.Value)
-	assert.Equal(
-		TagSet{
-			Tag{Name: "version", Value: "34.42"},
-			Tag{Name: "resource", Value: "/dash/list"},
-			Tag{Name: "service", Value: "dogweb"},
+		Span{
+			Duration: 0,
+			Metrics:  map[string]int64{"custom_size": 10},
 		},
-		c.TagSet,
-	)
-}
+	}
 
-func TestCountTimes(t *testing.T) {
-	assert := assert.New(t)
+	// add spans
+	var err error
+	for i := range testCounts {
+		for _, s := range spans {
+			c := testCounts[i]
+			testCounts[i], err = c.Add(s)
+			if c.Name == "custom_size" && s.Metrics == nil {
+				assert.NotNil(err)
+			} else {
+				assert.Nil(err)
+			}
+		}
+	}
 
-	tags := NewTagsFromString("version:34.42,resource:/dash/list,service:dogweb")
-	c := NewCount(DURATION, tags)
-
-	// Our fake span
-	s := Span{TraceID: RandomID(), SpanID: RandomID(), Start: Now(), Duration: 1e6}
-	c = c.Add(s)
-
-	assert.Equal(DURATION, c.Name)
-	assert.Equal(1000000, c.Value)
-	assert.Equal(
-		TagSet{
-			Tag{Name: "version", Value: "34.42"},
-			Tag{Name: "resource", Value: "/dash/list"},
-			Tag{Name: "service", Value: "dogweb"},
-		},
-		c.TagSet,
-	)
-}
-
-func TestCountBad(t *testing.T) {
-	assert := assert.New(t)
-	c := NewCount("raclette", TagSet{})
-	s := Span{TraceID: RandomID(), SpanID: RandomID(), Start: Now(), Duration: 1e6}
-	assert.Panics(func() { c.Add(s) })
+	for _, c := range testCounts {
+		switch c.Name {
+		case HITS:
+			assert.Equal(4, c.Value)
+		case ERRORS:
+			assert.Equal(1, c.Value)
+		case DURATION:
+			assert.Equal(50, c.Value)
+		case "custom_size":
+			assert.Equal(13, c.Value)
+		default:
+			t.Fatalf("Was not supposed to handle count %v", c)
+		}
+	}
 }
