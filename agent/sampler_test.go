8e8fd8873867578e315bd6e864b69292b3771748
Benjamin Fernandes <benjamin@datadoghq.com>
2015-09-14 11:59:47 +0200

    Avoid id=0 in tests

--- agent/sampler_test.go
+++ agent/sampler_test.go
@@ -16,16 +16,16 @@ func TestSampler(t *testing.T) {
 
 	// Add some spans to our sampler
 	testSpans := []model.Span{
-		model.Span{TraceID: 0, SpanID: 0, Duration: 573496},
-		model.Span{TraceID: 0, SpanID: 1, Duration: 992312},
-		model.Span{TraceID: 0, SpanID: 2, Duration: 769540},
-		model.Span{TraceID: 1, SpanID: 3, Duration: 26965},
-		model.Span{TraceID: 0, SpanID: 4, Duration: 513323},
-		model.Span{TraceID: 2, SpanID: 5, Duration: 34347},
-		model.Span{TraceID: 3, SpanID: 6, Duration: 498798},
-		model.Span{TraceID: 3, SpanID: 7, Duration: 19207},
-		model.Span{TraceID: 1, SpanID: 8, Duration: 197884},
-		model.Span{TraceID: 4, SpanID: 9, Duration: 151384},
+		model.Span{TraceID: 100, SpanID: 10, Duration: 573496},
+		model.Span{TraceID: 100, SpanID: 11, Duration: 992312},
+		model.Span{TraceID: 100, SpanID: 12, Duration: 769540},
+		model.Span{TraceID: 101, SpanID: 13, Duration: 26965},
+		model.Span{TraceID: 100, SpanID: 14, Duration: 513323},
+		model.Span{TraceID: 102, SpanID: 15, Duration: 34347},
+		model.Span{TraceID: 103, SpanID: 16, Duration: 498798},
+		model.Span{TraceID: 103, SpanID: 17, Duration: 19207},
+		model.Span{TraceID: 101, SpanID: 18, Duration: 197884},
+		model.Span{TraceID: 104, SpanID: 19, Duration: 151384},
 	}
 	for _, s := range testSpans {
 		sampler.AddSpan(s)
@@ -46,23 +46,23 @@ func TestSampler(t *testing.T) {
 	q1, samp1 := d.Summary.Quantile(0.5)
 	assert.Equal(int64(197884), q1)
 	assert.Equal(1, len(samp1))
-	assert.Equal(uint64(8), samp1[0])
+	assert.Equal(uint64(18), samp1[0])
 
 	q2, samp2 := d.Summary.Quantile(0.90)
 	assert.Equal(int64(769540), q2)
 	assert.Equal(1, len(samp2))
-	assert.Equal(uint64(2), samp2[0])
+	assert.Equal(uint64(12), samp2[0])
 
 	q3, samp3 := d.Summary.Quantile(0.99)
 	assert.Equal(int64(992312), q3)
 	assert.Equal(1, len(samp3))
-	assert.Equal(uint64(1), samp3[0])
+	assert.Equal(uint64(11), samp3[0])
 
 	// Add one fake distribution for choosing
 	stats.Distributions["whatever"] = d
 	chosen := sampler.GetSamples(stats, []float64{0.5, 0.90, 0.99})
 
-	shouldChoose := []int{0, 1, 2, 3, 4, 8}
+	shouldChoose := []int{10, 11, 12, 13, 14, 18}
 	chosenSID := make([]int, len(chosen))
 	for i, s := range chosen {
 		chosenSID[i] = int(s.SpanID)
