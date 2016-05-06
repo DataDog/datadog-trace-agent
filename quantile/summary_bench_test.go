c12706acd2a2c905738fd7ce92a3ef4b4a545409
Léo Cavaillé <leo@datadoghq.com>
2016-05-06 17:57:49 +0200

    simple summary backed by DLL in lieu of skiplist
    BenchmarkGKSkiplistRandom-4  1000000          1685 ns/op         146 B/op           4 allocs/op
    BenchmarkGKDLLRandom-4       1000000          1174 ns/op         112 B/op           3 allocs/op

---
+++ quantile/summary_bench_test.go
@@ -0,0 +1,43 @@
+package quantile
+
+import (
+	"math/rand"
+	"testing"
+)
+
+const randlen = 1000
+
+func randSlice() []float64 {
+	// use those
+	vals := make([]float64, 0, randlen)
+	for i := 0; i < randlen; i++ {
+		vals = append(vals, rand.Float64())
+	}
+
+	return vals
+}
+
+func BenchmarkGKSkiplistRandom(b *testing.B) {
+	s := NewSummary()
+
+	vals := randSlice()
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for n := 0; n < b.N; n++ {
+		s.Insert(vals[n%randlen], uint64(n))
+	}
+}
+
+func BenchmarkGKDLLRandom(b *testing.B) {
+	s := NewSimpleSummary()
+
+	vals := randSlice()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for n := 0; n < b.N; n++ {
+		s.Insert(vals[n%randlen], uint64(n))
+	}
+}
