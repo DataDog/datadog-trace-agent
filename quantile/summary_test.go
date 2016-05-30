381097ae6dea10135257d64c4e27f824d5aee319
Léo Cavaillé <leo@datadoghq.com>
2016-05-30 11:10:51 +0200

    [quantile/gk-slice] add slice summary and bench/tests
    └─$  benchcmp bench.skiplist bench.slice
    benchmark                        old ns/op     new ns/op     delta
    BenchmarkGKInsertion-4           1435          1894          +31.99%
    BenchmarkGKQuantiles10-4         64.3          60.4          -6.07%
    BenchmarkGKQuantiles100-4        270           230           -14.81%
    BenchmarkGKQuantiles1000-4       298           243           -18.46%
    BenchmarkGKQuantiles10000-4      361           248           -31.30%
    BenchmarkGKQuantiles100000-4     330           235           -28.79%
    BenchmarkGKEncoding10-4          42690         37801         -11.45%
    BenchmarkGKEncoding100-4         374153        331990        -11.27%
    BenchmarkGKEncoding1000-4        409947        367315        -10.40%
    
    benchmark                        old allocs     new allocs     delta
    BenchmarkGKInsertion-4           4              1              -75.00%
    BenchmarkGKQuantiles10-4         0              0              +0.00%
    BenchmarkGKQuantiles100-4        0              0              +0.00%
    BenchmarkGKQuantiles1000-4       0              0              +0.00%
    BenchmarkGKQuantiles10000-4      0              0              +0.00%
    BenchmarkGKQuantiles100000-4     0              0              +0.00%
    BenchmarkGKEncoding10-4          131            80             -38.93%
    BenchmarkGKEncoding100-4         959            635            -33.79%
    BenchmarkGKEncoding1000-4        1031           695            -32.59%
    
    benchmark                        old bytes     new bytes     delta
    BenchmarkGKInsertion-4           146           16            -89.04%
    BenchmarkGKQuantiles10-4         0             0             +0.00%
    BenchmarkGKQuantiles100-4        0             0             +0.00%
    BenchmarkGKQuantiles1000-4       0             0             +0.00%
    BenchmarkGKQuantiles10000-4      0             0             +0.00%
    BenchmarkGKQuantiles100000-4     0             0             +0.00%
    BenchmarkGKEncoding10-4          9578          5832          -39.11%
    BenchmarkGKEncoding100-4         80472         54264         -32.57%
    BenchmarkGKEncoding1000-4        82748         55720         -32.66%

--- quantile/summary_test.go
+++ quantile/summary_test.go
@@ -1,129 +1,186 @@
 package quantile
 
 import (
-	"encoding/json"
-	"math/rand"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-var TestArray = [...]float64{9193603307515863, 6754367334490737, 3319615827007736, 5889610372829623, 9056711574806808, 552001800400511, 13400109297856412, 9290419307253104, 22335189735651727, 20198755397426237, 6750759924055697, 5282418092338629, 7669954555439878, 9871073458267054, 9596813405908124, 17826985234078452, 20809685535785593, 169504538801585, 3742572289274816, 5269690153609658, 5911223635170287, 8307883610131084, 2782048181817819, 3730609951306336, 4049109332077119, 25526756656192197, 9178098692976052, 14247068431339682, 9790147915267598, 26879545051663945, 6061096718141231, 18513304861202883, 36038916238811566, 25585683826277574, 9896041109367753, 6071196089481084, 7192619690997925, 4638251828961559, 3042642059231756, 4695726824572235, 44887308998594665, 3933519497422833, 17471932249059073, 4220651987288255, 6037514377711865, 9334562578999125, 1564928248590489, 3263044748940436, 4590412317155934, 5470186478741421, 7945869119417566, 5954581154568589, 894968515670638, 7620414993900439, 9670709128536947, 6566981438432174, 7097246010376681, 38073653883035684, 28936241604793246, 9572137207232106, 8763202610627523, 24879815234751902, 29520475378617226, 4325698909679998, 1385395600985416, 21587377639944705, 23838639891325397, 47092556792851724, 380708162070026, 7041179813266126, 11313259703460376, 16338073267858633, 7157789354354156, 7679321360662391, 1296853853173733, 8289135153494715, 5067591593829185, 6875574194262736, 6816612766339816, 2744231634144031, 3371312301999041, 3582026995577731, 9560887595174091, 9478826709460911, 14554548042944748, 2381196435702264, 920004969814432, 16786031463171992, 9843289339356544, 4813972590910607, 8148484660815154, 3645114654798244, 57439089131166135, 8901361618305092, 1776157835648735, 5659433639754501, 43703315824233925, 760807861832865, 4575552020357635, 29239935027624495}
+/************************************************************************************
+	DATA VALIDATION, with different strategies make sure of the correctness of
+	our epsilon-approximate quantiles
+************************************************************************************/
 
-/*
-FIXME? Right now we're implementing the "lower" interpolation which is not the "linear" default one.
+var testQuantiles = []float64{0, 0.1, 0.25, 0.5, 0.75, 0.90, 0.95, 0.99, 0.999, 0.9999, 1}
 
-In [7]: np.percentile(TestArray, 50, interpolation='lower')
-Out[7]: 7192619690997925
+func GenSummarySkiplist(n int, gen func(i int) float64) ([]float64, []uint64) {
+	s := NewSummary()
 
-In [8]: np.percentile(TestArray, 95, interpolation='lower')
-Out[8]: 36038916238811566
+	for i := 0; i < n; i++ {
+		s.Insert(gen(i), uint64(i))
+	}
 
-In [9]: np.percentile(TestArray, 99, interpolation='lower')
-Out[9]: 47092556792851724
+	vals := make([]float64, 0, len(testQuantiles))
+	samps := make([]uint64, 0, len(testQuantiles))
+	for _, q := range testQuantiles {
+		val, samp := s.Quantile(q)
+		vals = append(vals, val)
+		samps = append(samps, samp...)
+	}
 
-In [10]: np.percentile(TestArray, 99.9, interpolation='lower')
-Out[10]: 47092556792851724
+	return vals, samps
+}
+
+func GenSummarySlice(n int, gen func(i int) float64) ([]float64, []uint64) {
+	s := NewSliceSummary()
+
+	for i := 0; i < n; i++ {
+		s.Insert(gen(i), uint64(i))
+	}
+
+	vals := make([]float64, 0, len(testQuantiles))
+	samps := make([]uint64, 0, len(testQuantiles))
+	for _, q := range testQuantiles {
+		val, samp := s.Quantile(q)
+		vals = append(vals, val)
+		samps = append(samps, samp...)
+	}
+
+	return vals, samps
+}
+
+/* CONSTANT STREAMS
+   The most simple checker
 */
+func ConstantGenerator(i int) float64 {
+	return 42
+}
+func SummarySkiplistConstantN(t *testing.T, n int) {
+	assert := assert.New(t)
+	vals, _ := GenSummarySkiplist(n, ConstantGenerator)
+	for _, v := range vals {
+		assert.Equal(42, v)
+	}
+}
+func SummarySliceConstantN(t *testing.T, n int) {
+	assert := assert.New(t)
+	vals, _ := GenSummarySlice(n, ConstantGenerator)
+	for _, v := range vals {
+		assert.Equal(42, v)
+	}
+}
+func TestSummarySkiplistConstant10(t *testing.T) {
+	SummarySkiplistConstantN(t, 10)
+}
+func TestSummarySkiplistConstant100(t *testing.T) {
+	SummarySkiplistConstantN(t, 100)
+}
+func TestSummarySkiplistConstant1000(t *testing.T) {
+	SummarySkiplistConstantN(t, 1000)
+}
+func TestSummarySkiplistConstant10000(t *testing.T) {
+	SummarySkiplistConstantN(t, 10000)
+}
+func TestSummarySkiplistConstant100000(t *testing.T) {
+	SummarySkiplistConstantN(t, 100000)
+}
+func TestSummarySliceConstant10(t *testing.T) {
+	SummarySliceConstantN(t, 10)
+}
+func TestSummarySliceConstant100(t *testing.T) {
+	SummarySliceConstantN(t, 100)
+}
+func TestSummarySliceConstant1000(t *testing.T) {
+	SummarySliceConstantN(t, 1000)
+}
+func TestSummarySliceConstant10000(t *testing.T) {
+	SummarySliceConstantN(t, 10000)
+}
+func TestSummarySliceConstant100000(t *testing.T) {
+	SummarySliceConstantN(t, 100000)
+}
 
-// NewSummaryWithTestData returns the Summary
+/* uniform distribution
+   expected quantiles are easily to compute as the value == its rank
+   1 to i
+*/
+func UniformGenerator(i int) float64 {
+	return float64(i)
+}
+func SummarySkiplistUniformN(t *testing.T, n int) {
+	assert := assert.New(t)
+	vals, _ := GenSummarySkiplist(n, UniformGenerator)
+
+	for i, v := range vals {
+		var exp float64
+		if testQuantiles[i] == 0 {
+			exp = 0
+		} else if testQuantiles[i] == 1 {
+			exp = float64(n) - 1
+		} else {
+			rank := math.Ceil(testQuantiles[i] * float64(n))
+			exp = rank - 1
+		}
+		assert.InDelta(exp, v, EPSILON*float64(n), "quantile %f failed, exp: %f, val: %f", testQuantiles[i], exp, v)
+	}
+}
+func SummarySliceUniformN(t *testing.T, n int) {
+	assert := assert.New(t)
+	vals, _ := GenSummarySlice(n, UniformGenerator)
+
+	for i, v := range vals {
+		var exp float64
+		if testQuantiles[i] == 0 {
+			exp = 0
+		} else if testQuantiles[i] == 1 {
+			exp = float64(n) - 1
+		} else {
+			rank := math.Ceil(testQuantiles[i] * float64(n))
+			exp = rank - 1
+		}
+		assert.InDelta(exp, v, EPSILON*float64(n), "quantile %f failed, exp: %f, val: %f", testQuantiles[i], exp, v)
+	}
+}
+func TestSummarySkiplistUniform10(t *testing.T) {
+	SummarySkiplistUniformN(t, 10)
+}
+func TestSummarySkiplistUniform100(t *testing.T) {
+	SummarySkiplistUniformN(t, 100)
+}
+func TestSummarySkiplistUniform1000(t *testing.T) {
+	SummarySkiplistUniformN(t, 1000)
+}
+func TestSummarySkiplistUniform10000(t *testing.T) {
+	SummarySkiplistUniformN(t, 10000)
+}
+func TestSummarySkiplistUniform100000(t *testing.T) {
+	SummarySkiplistUniformN(t, 100000)
+}
+func TestSummarySliceUniform10(t *testing.T) {
+	SummarySliceUniformN(t, 10)
+}
+func TestSummarySliceUniform100(t *testing.T) {
+	SummarySliceUniformN(t, 100)
+}
+func TestSummarySliceUniform1000(t *testing.T) {
+	SummarySliceUniformN(t, 1000)
+}
+func TestSummarySliceUniform10000(t *testing.T) {
+	SummarySliceUniformN(t, 10000)
+}
+func TestSummarySliceUniform100000(t *testing.T) {
+	SummarySliceUniformN(t, 100000)
+}
+
 func NewSummaryWithTestData() *Summary {
 	s := NewSummary()
 
-	for i, v := range TestArray {
-		s.Insert(v, uint64(i))
+	for i := 0; i < 1000; i++ {
+		s.Insert(float64(i), uint64(i))
 	}
 
 	return s
-}
-
-func TestSummaryInsertion(t *testing.T) {
-	assert := assert.New(t)
-
-	s := NewSummaryWithTestData()
-	assert.Equal(100, s.N)
-}
-
-type Quantile struct {
-	Value   float64
-	Samples []uint64
-}
-
-func TestSummaryQuantile(t *testing.T) {
-	assert := assert.New(t)
-
-	// For 100 elts and eps=0.01 the error on the rank is 0.01 * 100 = 1
-	// So we can have these results:
-	// *  7157789354354156, (SID=72)
-	// *  7192619690997925, (SID=36)
-	// *  7620414993900439, (SID=53)
-	s := NewSummaryWithTestData()
-
-	v, samples := s.Quantile(0.5)
-	// our sample array only yields a sample per value
-	assert.Equal(1, len(samples))
-	acceptable := []Quantile{
-		Quantile{Value: 7157789354354156, Samples: []uint64{72}},
-		Quantile{Value: 7192619690997925, Samples: []uint64{36}},
-		Quantile{Value: 7620414993900439, Samples: []uint64{53}},
-	}
-	foundCorrectQuantile := false
-	for _, q := range acceptable {
-		foundCorrectQuantile = q.Value == v && q.Samples[0] == samples[0]
-		if foundCorrectQuantile {
-			break
-		}
-	}
-
-	assert.True(foundCorrectQuantile, "Quantile %d (samples=%v) not found", v, samples)
-}
-
-func BenchmarkSummaryInsertion(b *testing.B) {
-	s := NewSummary()
-	for n := 0; n < b.N; n++ {
-		val := rand.Float64()
-		s.Insert(val, uint64(n))
-	}
-}
-
-func TestSummaryMarshal(t *testing.T) {
-	assert := assert.New(t)
-
-	s := NewSummaryWithTestData()
-
-	b, err := json.Marshal(s)
-	assert.Nil(err)
-
-	// Now test contents
-	ss := Summary{}
-	err = json.Unmarshal(b, &ss)
-
-	assert.Equal(s.N, ss.N)
-	v1, samp1 := s.Quantile(0.5)
-	v2, samp2 := ss.Quantile(0.5)
-
-	assert.Equal(v1, v2)
-	assert.Equal(1, len(samp1))
-	assert.Equal(1, len(samp2))
-
-	// Verify samples are correct
-	samp1Correct := false
-	for i, val := range TestArray {
-		if val == v1 && samp1[0] == uint64(i) {
-			samp1Correct = true
-			break
-		}
-	}
-	assert.True(samp1Correct, "1: sample %v incorrect for quantile %d", samp1, v1)
-
-	samp2Correct := false
-	for i, val := range TestArray {
-		if val == v2 && samp2[0] == uint64(i) {
-			samp2Correct = true
-			break
-		}
-	}
-	assert.True(samp2Correct, "2: sample %v incorrect for quantile %d", samp2, v2)
 }
 
 func TestSummaryGob(t *testing.T) {
@@ -165,30 +222,33 @@ func TestSummaryMerge(t *testing.T) {
 		v, _ := s1.Quantile(q)
 		assert.Equal(e, v)
 	}
-
 }
 
-// TestSummaryNonZeroMerge ensures we can safely merge samples with non-zero
-// values (these were previously causing a bug).
-func TestSummaryNonZeroMerge(t *testing.T) {
+func TestSummarySliceMerge(t *testing.T) {
 	assert := assert.New(t)
-	s1 := NewSummary()
-	s2 := NewSummary()
-	for i := 1; i < 6; i++ {
+	s1 := NewSliceSummary()
+	for i := 0; i < 101; i++ {
 		s1.Insert(float64(i), uint64(i))
+	}
+
+	s2 := NewSliceSummary()
+	for i := 0; i < 50; i++ {
 		s2.Insert(float64(i), uint64(i))
 	}
+
 	s1.Merge(s2)
 
-	v, _ := s1.Quantile(0)
-	assert.Equal(v, 1)
-	v, _ = s1.Quantile(0.5)
-	assert.Equal(v, 3)
-	v, _ = s1.Quantile(1)
-	assert.Equal(v, 5)
-}
+	expected := map[float64]int{
+		0.0: 0,
+		0.2: 15,
+		0.4: 30,
+		0.6: 45,
+		0.8: 70,
+		1.0: 100,
+	}
 
-func TestSummaryBySlices(t *testing.T) {
-	// Write a test!
-	t.Skip()
+	for q, e := range expected {
+		v, _ := s1.Quantile(q)
+		assert.Equal(e, v)
+	}
 }
