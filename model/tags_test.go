0e67b096d3cce8eb35cfc71fcf97878cc16b28cb
Christian Mauduit <christian.mauduit@datadoghq.com>
2016-09-22 10:31:34 -0400

    [trace] refactoring, removed usage of deprecated method
    raclette.NewTagsFromString -> raclette.NewTagSetFromString

--- model/tags_test.go
+++ model/tags_test.go
@@ -23,27 +23,27 @@ func TestGroup(t *testing.T) {
 }
 
 func TestSort(t *testing.T) {
-	t1 := NewTagsFromString("a:2,a:1,a:3")
-	t2 := NewTagsFromString("a:1,a:2,a:3")
+	t1 := NewTagSetFromString("a:2,a:1,a:3")
+	t2 := NewTagSetFromString("a:1,a:2,a:3")
 	sort.Sort(t1)
 	assert.Equal(t, t1, t2)
 }
 
 func TestTagMerge(t *testing.T) {
-	t1 := NewTagsFromString("a:1,a:2")
-	t2 := NewTagsFromString("a:2,a:3")
+	t1 := NewTagSetFromString("a:1,a:2")
+	t2 := NewTagSetFromString("a:2,a:3")
 	t3 := MergeTagSets(t1, t2)
-	assert.Equal(t, t3, NewTagsFromString("a:1,a:2,a:3"))
+	assert.Equal(t, t3, NewTagSetFromString("a:1,a:2,a:3"))
 
-	t1 = NewTagsFromString("a:1")
-	t2 = NewTagsFromString("a:2")
+	t1 = NewTagSetFromString("a:1")
+	t2 = NewTagSetFromString("a:2")
 	t3 = MergeTagSets(t1, t2)
-	assert.Equal(t, t3, NewTagsFromString("a:1,a:2"))
+	assert.Equal(t, t3, NewTagSetFromString("a:1,a:2"))
 
-	t1 = NewTagsFromString("a:2,a:1")
-	t2 = NewTagsFromString("a:6,a:2")
+	t1 = NewTagSetFromString("a:2,a:1")
+	t2 = NewTagSetFromString("a:6,a:2")
 	t3 = MergeTagSets(t1, t2)
-	assert.Equal(t, t3, NewTagsFromString("a:1,a:2,a:6"))
+	assert.Equal(t, t3, NewTagSetFromString("a:1,a:2,a:6"))
 
 }
 
