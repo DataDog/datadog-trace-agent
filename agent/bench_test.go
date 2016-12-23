213b0292bc61a4e8e81cdb636a0faaf333fc1943
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-12-23 15:40:51 +0100

    [benchmark] better acceptance benchmark

--- agent/bench_test.go
+++ agent/bench_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/DataDog/datadog-trace-agent/config"
@@ -8,9 +9,19 @@ import (
 )
 
 func BenchmarkAgentTraceProcessing(b *testing.B) {
+	// Disable debug logs in these tests
+	config.NewLoggerLevelCustom("INFO", "/var/log/datadog/trace-agent.log")
+
+	// TODO the seed must be passed using --seed flag so that executions
+	// are random but can be easily reproduced; using the default Seed(1)
+	// as a placeholder
+	rand.Seed(1)
+
 	conf := config.NewDefaultAgentConfig()
 	conf.APIKeys = append(conf.APIKeys, "")
 	agent := NewAgent(conf)
+	b.ResetTimer()
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		agent.Process(fixtures.RandomTrace())
 	}
