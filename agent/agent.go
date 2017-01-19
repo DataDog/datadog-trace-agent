DataDog/datadog-trace-agent

commit 3ee25af22bb2597cd505c2ed389664eb1e9949ed

Fix linting/vet error

agent/agent.go
@@ -88,13 +88,12 @@ func (a *Agent) Run() {
 
 			a.Writer.inPayloads <- p
 		case <-a.exit:
-			break
+			log.Info("exiting")
+			close(a.Receiver.exit)
+			close(a.Writer.exit)
+			a.Sampler.Stop()
 		}
 	}
-	log.Info("exiting")
-	close(a.Receiver.exit)
-	close(a.Writer.exit)
-	a.Sampler.Stop()
 }
 
 // Process is the default work unit that receives a trace, transforms it and

commit e6415f1d0a2c3d751c73bd539a465986fccc5ae7

fix panic when exiting
3ee25af2 introduced a bug by not exiting the main loop after closing the
exit chan. Therefore the program panics when close is called a second
time.

We also use (*Writer).Stop() since it exists.

agent/agent.go
@@ -90,8 +90,9 @@ func (a *Agent) Run() {
 		case <-a.exit:
 			log.Info("exiting")
 			close(a.Receiver.exit)
-			close(a.Writer.exit)
+			a.Writer.Stop()
 			a.Sampler.Stop()
+			return
 		}
 	}
 }