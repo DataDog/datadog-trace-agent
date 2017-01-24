8cb598669e32cba7787fea536a4e352b778a3b2a
Nicolas Martyanoff <nicolas.martyanoff@datadoghq.com>
2017-01-24 17:41:41 +0100

    check the root span date before computing sublayers
    Because computing sublayers is expensive.

--- agent/agent.go
+++ agent/agent.go
@@ -108,16 +108,16 @@ func (a *Agent) Process(t model.Trace) {
 		return
 	}
 
-	sublayers := model.ComputeSublayers(&t)
 	root := t.GetRoot()
-	model.SetSublayersOnSpan(root, sublayers)
-
 	if root.End() < model.Now()-2*a.conf.BucketInterval.Nanoseconds() {
 		log.Debugf("skipping trace with root too far in past, root:%v", *root)
 		atomic.AddInt64(&a.Receiver.stats.TracesDropped, 1)
 		atomic.AddInt64(&a.Receiver.stats.SpansDropped, int64(len(t)))
 		return
 	}
+
+	sublayers := model.ComputeSublayers(&t)
+	model.SetSublayersOnSpan(root, sublayers)
 
 	for i := range t {
 		t[i] = quantizer.Quantize(t[i])
