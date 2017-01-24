c05fcdd32d5c5fe535ce7e87f9478beb2b66c2fb
Nicolas Martyanoff <nicolas.martyanoff@datadoghq.com>
2017-01-24 16:28:18 +0100

    do not reuse the same object for span tests
    When a structure is copied, all its member are copied. But maps are
    actually reference, so the reference is copied, not the map and its
    content. The result was that some tests modified testSpan.Meta and
    testSpan.Metrics. Not good.

--- model/span_test.go
+++ model/span_test.go
@@ -6,29 +6,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var testSpan = Span{
-	Duration: 10000000,
-	Error:    0,
-	Resource: "GET /some/raclette",
-	Service:  "django",
-	Name:     "django.controller",
-	SpanID:   42,
-	Start:    1448466874000000000,
-	TraceID:  424242,
-	Meta: map[string]string{
-		"user": "leo",
-		"pool": "fondue",
-	},
-	Metrics: map[string]float64{
-		"cheese_weight": 100000.0,
-	},
-	ParentID: 1111,
-	Type:     "http",
+func testSpan() Span {
+	return Span{
+		Duration: 10000000,
+		Error:    0,
+		Resource: "GET /some/raclette",
+		Service:  "django",
+		Name:     "django.controller",
+		SpanID:   42,
+		Start:    1448466874000000000,
+		TraceID:  424242,
+		Meta: map[string]string{
+			"user": "leo",
+			"pool": "fondue",
+		},
+		Metrics: map[string]float64{
+			"cheese_weight": 100000.0,
+		},
+		ParentID: 1111,
+		Type:     "http",
+	}
 }
 
 func TestSpanString(t *testing.T) {
 	assert := assert.New(t)
-	assert.NotEqual("", testSpan.String())
+	assert.NotEqual("", testSpan().String())
 }
 
 func TestSpanFlushMarker(t *testing.T) {
