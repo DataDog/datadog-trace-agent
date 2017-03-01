707f005862e1f4a4abf285dfb7c61b3d2dd37549
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2017-03-01 15:15:38 +0100

    [sampling] added a func to guess pre-sample rate

--- sampler/presampler.go
+++ sampler/presampler.go
@@ -149,3 +149,63 @@ func (ps *PreSampler) Sample(req *http.Request) bool {
 
 	return keep
 }
+
+// CalcPreSampleRate gives the new sample rate to apply for a given max user CPU average.
+// It takes the current sample rate and user CPU average as those parameters both
+// have an influence on the result.
+func CalcPreSampleRate(maxUserAvg, currentUserAvg, currentRate float64) float64 {
+	const (
+		// userAvg0 is the CPU usage we can possibly reach with sampling at 0%.
+		// Of course this really depends on the data received, the context,
+		// the machine running the code etc. But, there's no point in targetting 0.
+		// Benchmarks show 2% should always be, reasonnably, reachable.
+		userAvg0 = float64(0.02) // 2% CPU usage
+		// userAvgMin is a limit that maxUserAvg should respect, because trying
+		// to remain below this through pre-sampling can do more harm than good,
+		// trying to drop everything and still not reaching the goal.
+		userAvgMin = float64(0.04) // 4% CPU usage
+		// deltaMin is a threshold that must be passed before changing the
+		// pre-sampling rate. If set to 0.3, for example, the new rate must be
+		// either over 130% or below 70% of the previous value, before we actually
+		// adjust the sampling rate. This is to avoid over-adapting and jittering.
+		deltaMin = float64(0.3) // +/- 30% change
+		// rateMin is an absolute minimum rate, never sample more than this, it is
+		// inefficient, the cost handling the payloads without even reading them
+		// is too high anyway.
+		rateMin = float64(0.05) // 5% hard-limit
+	)
+
+	if maxUserAvg <= userAvg0 || currentUserAvg <= 0 || currentRate <= 0 || currentRate > 1 {
+		return 1 // inconsistent input data, in doubt, disable the feature
+	}
+
+	if maxUserAvg < userAvgMin {
+		maxUserAvg = userAvgMin
+	}
+
+	newRate := float64(1)
+	slope := (currentUserAvg - userAvg0) / currentRate
+	if slope <= 0 {
+		// OK, here, slope is:
+		// - zero -> no matter how we presample, CPU will remain the same
+		// - negative -> would mean the more we sample, the more CPU we consume...
+		// So in short, there's no need to sample (in practice, it means
+		// we're below userAvg0, which is our base CPU usage).
+		return 1
+	}
+
+	newRate = (maxUserAvg - userAvg0) / slope
+	if newRate <= 0 || newRate >= 1 {
+		return 1 // again, in doubt, disable pre-sampling
+	}
+
+	delta := 2 * (newRate - currentRate) / (newRate + currentRate)
+	if delta > -deltaMin && delta < 0 {
+		return currentRate
+	}
+	if newRate < rateMin {
+		return rateMin
+	}
+
+	return newRate
+}
