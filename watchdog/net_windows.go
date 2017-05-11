d13b6c52b3559a28a79d8d9ce3e14b50cbb0d272
Raphael Gavache <raphael.gavache@student.ecp.fr>
2017-05-11 11:59:57 -0400

    keeping just NetInfo connections count at 0

--- watchdog/net_windows.go
+++ watchdog/net_windows.go
@@ -1,20 +1,6 @@
 package watchdog
 
-import (
-	"time"
-)
-
 // Net for windows returns basic network info without the number of connections.
 func (pi *CurrentInfo) Net() NetInfo {
-	pi.mu.Lock()
-	defer pi.mu.Unlock()
-
-	now := time.Now()
-	dt := now.Sub(pi.lastNetTime)
-	if dt <= pi.cacheDelay {
-		return pi.lastNet // don't query too often, cache a little bit
-	}
-	pi.lastNetTime = now
-
-	return pi.lastNet
+	return NetInfo{}
 }
