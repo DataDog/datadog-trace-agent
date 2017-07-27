667572e9cabfff556331b260aad7570fdc9a699a
gabsn <gabin.marignier@datadoghq.com>
2017-07-27 12:35:54 -0400

    Remove debug printings

--- agent/main.go
+++ agent/main.go
@@ -180,7 +180,6 @@ func main() {
 	if opts.info {
 		if err := Info(os.Stdout, agentConf); err != nil {
 			// need not display the error, Info should do it already
-			println(err.Error())
 			os.Exit(1)
 		}
 		return
