7ca9ef57c9abe30483aad7d4b6d5a5589192c078
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-02-09 01:08:04 -0500

    Abstract apm_configs to functions

--- config/config_yaml.go
+++ config/config_yaml.go
@@ -5,12 +5,60 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"strings"
+	"time"
+
+	"github.com/DataDog/datadog-trace-agent/backoff"
+	writerconfig "github.com/DataDog/datadog-trace-agent/writer/config"
 
 	"github.com/DataDog/datadog-trace-agent/utils"
 )
 
 // YamlAgentConfig is a structure used for marshaling the datadog.yaml configuration
 // available in Agent versions >= 6
+
+type traceWriter struct {
+	MaxSpansPerPayload     int                    `yaml:"max_spans_per_payload"`
+	FlushPeriod            int                    `yaml:"flush_period_seconds"`
+	UpdateInfoPeriod       int                    `yaml:"update_info_period_seconds"`
+	QueueablePayloadSender queueablePayloadSender `yaml:"queueable_payload_sender"`
+}
+
+type serviceWriter struct {
+	UpdateInfoPeriod       int                    `yaml:"update_info_period_seconds"`
+	FlushPeriod            int                    `yaml:"flush_period_seconds"`
+	QueueablePayloadSender queueablePayloadSender `yaml:"queueable_payload_sender"`
+}
+
+type statsWriter struct {
+	UpdateInfoPeriod       int                    `yaml:"update_info_period_seconds"`
+	QueueablePayloadSender queueablePayloadSender `yaml:"queueable_payload_sender"`
+}
+
+type queueablePayloadSender struct {
+	MaxAge            int   `yaml:"queue_max_age_seconds"`
+	MaxQueuedBytes    int64 `yaml:"queue_max_bytes"`
+	MaxQueuedPayloads int   `yaml:"queue_max_payloads"`
+	BackoffDuration   int   `yaml:"exp_backoff_max_duration_seconds"`
+	BackoffBase       int   `yaml:"exp_backoff_base_milliseconds"`
+	BackoffGrowth     int   `yaml:"exp_backoff_growth_base"`
+}
+
+type traceAgent struct {
+	Enabled            bool    `yaml:"enabled"`
+	Env                string  `yaml:"env"`
+	ExtraSampleRate    float64 `yaml:"extra_sample_rate"`
+	MaxTracesPerSecond float64 `yaml:"max_traces_per_second"`
+	Ignore             string  `yaml:"ignore_resource"`
+	ReceiverPort       int     `yaml:"receiver_port"`
+	ConnectionLimit    int     `yaml:"connection_limit"`
+	NonLocalTraffic    string  `yaml:"trace_non_local_traffic"`
+
+	TraceWriter   traceWriter   `yaml:"trace_writer"`
+	ServiceWriter serviceWriter `yaml:"service_writer"`
+	StatsWriter   statsWriter   `yaml:"stats_writer"`
+}
+
+//YamlAgentConfig is the Primary Object we retrieve from Datadog.yaml
 type YamlAgentConfig struct {
 	APIKey   string `yaml:"api_key"`
 	HostName string `yaml:"hostname"`
@@ -23,48 +71,7 @@ type YamlAgentConfig struct {
 
 	DefaultEnv string `yaml:"env"`
 
-	TraceAgent struct {
-		Enabled            bool    `yaml:"enabled"`
-		Env                string  `yaml:"env"`
-		ExtraSampleRate    float64 `yaml:"extra_sample_rate"`
-		MaxTracesPerSecond float64 `yaml:"max_traces_per_second"`
-		Ignore             string  `yaml:"ignore_resource"`
-		ReceiverPort       int     `yaml:"receiver_port"`
-		ConnectionLimit    int     `yaml:"connection_limit"`
-		NonLocalTraffic    string  `yaml:"trace_non_local_traffic"`
-
-		//TODO Merge these into config
-		TraceWriter struct {
-			MaxSpansPerPayload int   `yaml:"max_spans_per_payload"`
-			FlushPeriod        int   `yaml:"flush_period_seconds"`
-			UpdateInfoPeriod   int   `yaml:"update_info_period_seconds"`
-			MaxAge             int   `yaml:"queue_max_age_seconds"`
-			MaxQueuedBytes     int64 `yaml:"queue_max_bytes"`
-			MaxQueuedPayloads  int   `yaml:"queue_max_payloads"`
-			BackoffDuration    int   `yaml:"exp_backoff_max_duration_seconds"`
-			BackoffBase        int   `yaml:"exp_backoff_base_milliseconds"`
-			BackoffGrowth      int   `yaml:"exp_backoff_growth_base"`
-		} `yaml:"trace_writer"`
-		ServiceWriter struct {
-			FlushPeriod       int   `yaml:"flush_period_seconds"`
-			UpdateInfoPeriod  int   `yaml:"'update_info_period_seconds"`
-			MaxAge            int   `yaml:"queue_max_age_seconds"`
-			MaxQueuedBytes    int64 `yaml:"queue_max_bytes"`
-			MaxQueuedPayloads int   `yaml:"queue_max_payloads"`
-			BackoffDuration   int   `yaml:"exp_backoff_max_duration_seconds"`
-			BackoffBase       int   `yaml:"exp_backoff_base_milliseconds"`
-			BackoffGrowth     int   `yaml:"exp_backoff_growth_base"`
-		} `yaml:"service_writer"`
-		StatsWriter struct {
-			UpdateInfoPeriod  int   `yaml:"update_info_period_seconds"`
-			MaxAge            int   `yaml:"queue_max_age_seconds"`
-			MaxQueuedBytes    int64 `yaml:"queue_max_bytes"`
-			MaxQueuedPayloads int   `yaml:"queue_max_payloads"`
-			BackoffDuration   int   `yaml:"exp_backoff_max_duration_seconds"`
-			BackoffBase       int   `yaml:"exp_backoff_base_milliseconds"`
-			BackoffGrowth     int   `yaml:"exp_backoff_growth_base"`
-		} `yaml:"stats_writer"`
-	} `yaml:"trace_config"`
+	TraceAgent traceAgent `yaml:"apm_config"`
 }
 
 // NewYamlIfExists returns a new YamlAgentConfig if the given configPath is exists.
@@ -112,35 +119,89 @@ func mergeYamlConfig(agentConf *AgentConfig, yc *YamlAgentConfig) (*AgentConfig,
 	agentConf.StatsdHost = yc.StatsdHost
 	agentConf.ReceiverHost = yc.ReceiverHost
 
-	//Trace Writer
-	agentConf.TraceWriterConfig.FlushPeriod = utils.GetDuration(yc.TraceAgent.TraceWriter.FlushPeriod)
-	agentConf.TraceWriterConfig.MaxSpansPerPayload = yc.TraceAgent.TraceWriter.MaxSpansPerPayload
-	agentConf.TraceWriterConfig.UpdateInfoPeriod = utils.GetDuration(yc.TraceAgent.TraceWriter.UpdateInfoPeriod)
-	agentConf.TraceWriterConfig.SenderConfig.MaxAge = utils.GetDuration(yc.TraceAgent.TraceWriter.MaxAge)
-	agentConf.TraceWriterConfig.SenderConfig.MaxQueuedBytes = yc.TraceAgent.TraceWriter.MaxQueuedBytes
-	agentConf.TraceWriterConfig.SenderConfig.MaxQueuedPayloads = yc.TraceAgent.TraceWriter.MaxQueuedPayloads
-	agentConf.TraceWriterConfig.SenderConfig.ExponentialBackoff.MaxDuration = utils.GetDuration(yc.TraceAgent.TraceWriter.BackoffDuration)
-	agentConf.TraceWriterConfig.SenderConfig.ExponentialBackoff.Base = utils.GetDuration(yc.TraceAgent.TraceWriter.BackoffBase)
-	agentConf.TraceWriterConfig.SenderConfig.ExponentialBackoff.GrowthBase = yc.TraceAgent.TraceWriter.BackoffGrowth
-
-	//Service Writer
-	agentConf.ServiceWriterConfig.FlushPeriod = utils.GetDuration(yc.TraceAgent.ServiceWriter.FlushPeriod)
-	agentConf.ServiceWriterConfig.UpdateInfoPeriod = utils.GetDuration(yc.TraceAgent.ServiceWriter.UpdateInfoPeriod)
-	agentConf.ServiceWriterConfig.SenderConfig.MaxAge = utils.GetDuration(yc.TraceAgent.ServiceWriter.MaxAge)
-	agentConf.ServiceWriterConfig.SenderConfig.MaxQueuedBytes = yc.TraceAgent.ServiceWriter.MaxQueuedBytes
-	agentConf.ServiceWriterConfig.SenderConfig.MaxQueuedPayloads = yc.TraceAgent.ServiceWriter.MaxQueuedPayloads
-	agentConf.ServiceWriterConfig.SenderConfig.ExponentialBackoff.MaxDuration = utils.GetDuration(yc.TraceAgent.ServiceWriter.BackoffDuration)
-	agentConf.ServiceWriterConfig.SenderConfig.ExponentialBackoff.Base = utils.GetDuration(yc.TraceAgent.ServiceWriter.BackoffBase)
-	agentConf.ServiceWriterConfig.SenderConfig.ExponentialBackoff.GrowthBase = yc.TraceAgent.ServiceWriter.BackoffGrowth
-
-	//Stats Writer
-	agentConf.StatsWriterConfig.UpdateInfoPeriod = utils.GetDuration(yc.TraceAgent.StatsWriter.UpdateInfoPeriod)
-	agentConf.StatsWriterConfig.SenderConfig.MaxAge = utils.GetDuration(yc.TraceAgent.StatsWriter.MaxAge)
-	agentConf.StatsWriterConfig.SenderConfig.MaxQueuedBytes = yc.TraceAgent.StatsWriter.MaxQueuedBytes
-	agentConf.StatsWriterConfig.SenderConfig.MaxQueuedPayloads = yc.TraceAgent.StatsWriter.MaxQueuedPayloads
-	agentConf.StatsWriterConfig.SenderConfig.ExponentialBackoff.MaxDuration = utils.GetDuration(yc.TraceAgent.StatsWriter.BackoffDuration)
-	agentConf.StatsWriterConfig.SenderConfig.ExponentialBackoff.Base = utils.GetDuration(yc.TraceAgent.StatsWriter.BackoffBase)
-	agentConf.StatsWriterConfig.SenderConfig.ExponentialBackoff.GrowthBase = yc.TraceAgent.StatsWriter.BackoffGrowth
-
+	agentConf.ServiceWriterConfig = readServiceWriterConfigYaml(yc.TraceAgent.ServiceWriter)
+	agentConf.StatsWriterConfig = readStatsWriterConfigYaml(yc.TraceAgent.StatsWriter)
+	agentConf.TraceWriterConfig = readTraceWriterConfigYaml(yc.TraceAgent.TraceWriter)
 	return agentConf, nil
+}
+
+func readServiceWriterConfigYaml(yc serviceWriter) writerconfig.ServiceWriterConfig {
+	c := writerconfig.DefaultServiceWriterConfig()
+
+	if yc.FlushPeriod > 0 {
+		c.FlushPeriod = utils.GetDuration(yc.FlushPeriod)
+	}
+
+	if yc.UpdateInfoPeriod > 0 {
+		c.UpdateInfoPeriod = utils.GetDuration(yc.UpdateInfoPeriod)
+	}
+
+	c.SenderConfig = readQueueablePayloadSenderConfigYaml(yc.QueueablePayloadSender)
+	return c
+}
+
+func readStatsWriterConfigYaml(yc statsWriter) writerconfig.StatsWriterConfig {
+	c := writerconfig.DefaultStatsWriterConfig()
+
+	if yc.UpdateInfoPeriod > 0 {
+		c.UpdateInfoPeriod = utils.GetDuration(yc.UpdateInfoPeriod)
+	}
+
+	c.SenderConfig = readQueueablePayloadSenderConfigYaml(yc.QueueablePayloadSender)
+
+	return c
+}
+
+func readTraceWriterConfigYaml(yc traceWriter) writerconfig.TraceWriterConfig {
+	c := writerconfig.DefaultTraceWriterConfig()
+
+	if yc.MaxSpansPerPayload > 0 {
+		c.UpdateInfoPeriod = utils.GetDuration(yc.MaxSpansPerPayload)
+	}
+	if yc.FlushPeriod > 0 {
+		c.FlushPeriod = utils.GetDuration(yc.FlushPeriod)
+	}
+	if yc.UpdateInfoPeriod > 0 {
+		c.UpdateInfoPeriod = utils.GetDuration(yc.UpdateInfoPeriod)
+	}
+
+	c.SenderConfig = readQueueablePayloadSenderConfigYaml(yc.QueueablePayloadSender)
+
+	return c
+}
+
+func readQueueablePayloadSenderConfigYaml(yc queueablePayloadSender) writerconfig.QueuablePayloadSenderConf {
+	c := writerconfig.DefaultQueuablePayloadSenderConf()
+
+	if yc.MaxAge > 0 {
+		c.MaxAge = utils.GetDuration(yc.MaxAge)
+	}
+
+	if yc.MaxQueuedBytes > 0 {
+		c.MaxQueuedBytes = yc.MaxQueuedBytes
+	}
+
+	if yc.MaxQueuedPayloads > 0 {
+		c.MaxQueuedPayloads = yc.MaxQueuedPayloads
+	}
+
+	c.ExponentialBackoff = readExponentialBackoffConfigYaml(yc)
+
+	return c
+}
+
+func readExponentialBackoffConfigYaml(yc queueablePayloadSender) backoff.ExponentialConfig {
+	c := backoff.DefaultExponentialConfig()
+
+	if yc.BackoffDuration > 0 {
+		c.MaxDuration = utils.GetDuration(yc.BackoffDuration)
+	}
+	if yc.BackoffBase > 0 {
+		c.Base = time.Duration(yc.BackoffBase) * time.Millisecond
+	}
+	if yc.BackoffGrowth > 0 {
+		c.GrowthBase = yc.BackoffGrowth
+	}
+
+	return c
 }
