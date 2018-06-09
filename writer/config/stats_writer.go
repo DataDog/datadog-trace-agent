95fe63095fd0e352ddaa3e240b55ae7f3de0ee37
Matthieu Vlad Hauglustaine <vlad@datadoghq.com>
2018-06-09 13:56:59 +0200

    config/writer: set maxEntriesPerPayload to 12000.
    This allows us to stay under 1.5MB for our compressed stats payloads.

--- writer/config/stats_writer.go
+++ writer/config/stats_writer.go
@@ -2,10 +2,12 @@ package config
 
 import "time"
 
-// maxEntriesPerPayload is the maximum number of entries in a stat payload. A
-// value of 0 disables splitting all together. For now, the feature is disabled
-// by default.
-const maxEntriesPerPayload = 0
+// maxEntriesPerPayload is the maximum number of entries in a stat payload. An
+// entry has an average size of 125 bytes in a compressed payload. The current
+// Datadog intake API limits a compressed payload to ~3MB (24,000 entries), but
+// let's have the default ensure we don't have paylods > 1.5 MB (12,000
+// entries).
+const maxEntriesPerPayload = 12000
 
 // StatsWriterConfig contains the configuration to customize the behaviour of a TraceWriter.
 type StatsWriterConfig struct {
