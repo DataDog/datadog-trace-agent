f89f36424f6613176d60b7a5a84eda8612812b2d
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2018-07-16 04:58:21 -0400

    quantile: remove dead code (#453)

--- quantile/summary.go
+++ quantile/summary.go
@@ -1,16 +1,10 @@
 package quantile
 
 import (
-	"bytes"
-	"encoding/gob"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"math/rand"
 )
 
 /*
-DEPRECATED: this code is not used anymore, SliceSummary is used instead
 "Space-Efficient Online Computation of Quantile Summaries" (Greenwald, Khanna 2001)
 
 http://infolab.stanford.edu/~datar/courses/cs361a/papers/quantiles.pdf
@@ -23,13 +17,6 @@ summary faster.  Querying is still O(n).
 // EPSILON is the precision of the rank returned by our quantile queries
 const EPSILON float64 = 0.01
 
-// Summary is a way to represent an approximation of the distribution of values
-type Summary struct {
-	data        *Skiplist // where the real data is stored
-	EncodedData []Entry   `json:"data"` // flattened data user for ser/deser purposes
-	N           int       `json:"n"`    // number of unique points that have been added to this summary
-}
-
 // Entry is an element of the skiplist, see GK paper for description
 type Entry struct {
 	V     float64 `json:"v"`
@@ -37,243 +24,11 @@ type Entry struct {
 	Delta int     `json:"delta"`
 }
 
-// NewSummary returns a new approx-summary with accuracy EPSILON
-func NewSummary() *Summary {
-	return &Summary{
-		data: NewSkiplist(),
-	}
-}
-
-func (s Summary) String() string {
-	var b bytes.Buffer
-	b.WriteString(fmt.Sprintf("samples: %d\n", s.N))
-	curr := s.data.head.next[0]
-	i := 0
-
-	for curr != nil {
-		e := curr.value
-		b.WriteString(fmt.Sprintf("v:%6.02f g:%05d d:%05d   ", e.V, e.G, e.Delta))
-		if i%10 == 9 {
-			b.WriteRune('\n')
-		}
-		i++
-		curr = curr.next[0]
-	}
-	return b.String()
-}
-
-// MarshalJSON is used to send the data over to the API
-func (s Summary) MarshalJSON() ([]byte, error) {
-	if s.data == nil {
-		panic(errors.New("Cannot marshal non-initialized Summary"))
-	}
-
-	// TODO[leo] preallocate, not sure: 1/ 2*EPSILON?
-	s.EncodedData = make([]Entry, 0)
-	curr := s.data.head.next[0]
-	for curr != nil {
-		s.EncodedData = append(s.EncodedData, curr.value)
-		curr = curr.next[0]
-	}
-
-	return json.Marshal(map[string]interface{}{
-		"data": s.EncodedData,
-		"n":    s.N,
-	})
-}
-
-// Avoid infinite recursion when unmarshalling
-// When unmarshalling bytes in a Summary{} struct, it will call UnmarshalJSON recursively because Summary{} implements the unmarshaller interface
-// using the private type summary here, tricks the unmarshaller into running the regular JSON unmarshalling.
-type summary Summary
-
-// UnmarshalJSON is used to recreate a Summary structure from a JSON payload
-// It reinserts points artificially (TODO: see if this is OK?)
-func (s *Summary) UnmarshalJSON(b []byte) error {
-	ss := summary{}
-	err := json.Unmarshal(b, &ss)
-	if err != nil {
-		return err
-	}
-	*s = Summary(ss)
-
-	s.data = NewSkiplist()
-	for _, e := range s.EncodedData {
-		s.data.Insert(e)
-	}
-
-	return nil
-}
-
-// GobEncode is used by the Kafka payload now, it flattens our skiplist
-func (s *Summary) GobEncode() ([]byte, error) {
-	// TODO[leo] preallocate, not sure: 1/ 2*EPSILON?
-	s.EncodedData = make([]Entry, 0)
-	curr := s.data.head.next[0]
-	for curr != nil {
-		s.EncodedData = append(s.EncodedData, curr.value)
-		curr = curr.next[0]
-	}
-	ss := summary(*s)
-
-	var buf bytes.Buffer
-	encoder := gob.NewEncoder(&buf)
-	err := encoder.Encode(ss)
-	return buf.Bytes(), err
-}
-
-// GobDecode recreates a skiplist, TODO[leo] is the skiplist recreated as is?
-func (s *Summary) GobDecode(data []byte) error {
-	ss := summary{}
-	buf := bytes.NewBuffer(data)
-	decoder := gob.NewDecoder(buf)
-	if err := decoder.Decode(&ss); err != nil {
-		return err
-	}
-
-	*s = Summary(ss)
-	s.data = NewSkiplist()
-	for _, e := range s.EncodedData {
-		s.data.Insert(e)
-	}
-
-	return nil
-}
-
-// Insert inserts a new value v in the summary paired with t (the ID of the span it was reported from)
-func (s *Summary) Insert(v float64, t uint64) {
-	e := Entry{
-		V:     v,
-		G:     1,
-		Delta: 0,
-	}
-
-	eptr := s.data.Insert(e)
-
-	s.N++
-
-	if eptr.prev[0] != s.data.head && eptr.next[0] != nil {
-		eptr.value.Delta = int(2 * EPSILON * float64(s.N))
-	}
-
-	if s.N%int(1.0/float64(2.0*EPSILON)) == 0 {
-		s.compress()
-	}
-}
-
-func (s *Summary) compress() {
-	var missing int
-	epsN := int(2 * EPSILON * float64(s.N))
-
-	// keep first and last element
-	for elt := s.data.head.next[0]; elt != nil && elt.next[0] != nil; {
-		next := elt.next[0]
-		t := elt.value
-		nt := &next.value
-
-		// value merging
-		if t.V == nt.V {
-			missing += nt.G
-			nt.Delta += missing
-			nt.G = t.G
-			s.data.Remove(elt)
-		} else if elt != s.data.head.next[0] && next != nil {
-			if t.G+nt.G+missing+nt.Delta < epsN {
-				nt.G += t.G + missing
-				missing = 0
-				s.data.Remove(elt)
-			} else {
-				nt.G += missing
-				missing = 0
-			}
-		}
-
-		elt = next
-	}
-}
-
-// Quantile returns an EPSILON estimate of the element at quantile 'q' (0 <= q <= 1)
-func (s *Summary) Quantile(q float64) float64 {
-	// convert quantile to rank
-	r := int(q*float64(s.N) + 0.5)
-	epsN := int(EPSILON * float64(s.N))
-	var rmin int
-
-	for elt := s.data.head.next[0]; elt != nil; elt = elt.next[0] {
-		t := elt.value
-		rmin += t.G
-		n := elt.next[0]
-
-		if n == nil {
-			return t.V
-		}
-
-		if r+epsN < rmin+n.value.G+n.value.Delta {
-			if r+epsN < rmin+n.value.G {
-				return t.V
-			}
-			return n.value.V
-		}
-	}
-
-	panic("not reached")
-}
-
 // SummarySlice reprensents how many values are in a [Start, End] range
 type SummarySlice struct {
 	Start  float64
 	End    float64
 	Weight int
-}
-
-// BySlices returns a slice of Summary slices that represents weighted ranges of
-// values
-// e.g.    [0, 1]  : 3
-//		   [1, 23] : 12 ...
-// The number of intervals is related to the precision kept in the internal
-// data structure to ensure epsilon*s.N precision on quantiles, but it's bounded.
-// The weights are not exact, they're only upper bounds (see GK paper).
-func (s *Summary) BySlices() []SummarySlice {
-	var slices []SummarySlice
-
-	last := s.data.head
-	cur := last.next[0]
-
-	for cur != nil {
-		ss := SummarySlice{
-			Start:  last.value.V,
-			End:    cur.value.V,
-			Weight: cur.value.G,
-		}
-		slices = append(slices, ss)
-
-		last = cur
-		cur = cur.next[0]
-	}
-
-	return slices
-}
-
-// Merge takes a summary and merge the values inside the current pointed object
-func (s *Summary) Merge(s2 *Summary) {
-	if s2.N == 0 || s2.data == nil {
-		return
-	}
-
-	s.N += s2.N
-	// Iterate on s2 elements and insert/merge them
-	for elt := s2.data.head.next[0]; elt != nil; elt = elt.next[0] {
-		s.data.Insert(elt.value)
-	}
-	// Force compression
-	s.compress()
-}
-
-// Copy just returns a new summary with the same data
-func (s *Summary) Copy() *Summary {
-	other := NewSummary()
-	other.Merge(s) // cheez
-	return other
 }
 
 const maxHeight = 31
--- quantile/summary_test.go
+++ quantile/summary_test.go
@@ -15,22 +15,6 @@ import (
 
 var testQuantiles = []float64{0, 0.1, 0.25, 0.5, 0.75, 0.90, 0.95, 0.99, 0.999, 0.9999, 1}
 
-func GenSummarySkiplist(n int, gen func(i int) float64) []float64 {
-	s := NewSummary()
-
-	for i := 0; i < n; i++ {
-		s.Insert(gen(i), uint64(i))
-	}
-
-	vals := make([]float64, 0, len(testQuantiles))
-	for _, q := range testQuantiles {
-		val := s.Quantile(q)
-		vals = append(vals, val)
-	}
-
-	return vals
-}
-
 func GenSummarySlice(n int, gen func(i int) float64) []float64 {
 	s := NewSliceSummary()
 
@@ -53,34 +37,12 @@ func GenSummarySlice(n int, gen func(i int) float64) []float64 {
 func ConstantGenerator(i int) float64 {
 	return 42
 }
-func SummarySkiplistConstantN(t *testing.T, n int) {
-	assert := assert.New(t)
-	vals := GenSummarySkiplist(n, ConstantGenerator)
-	for _, v := range vals {
-		assert.Equal(42.0, v)
-	}
-}
 func SummarySliceConstantN(t *testing.T, n int) {
 	assert := assert.New(t)
 	vals := GenSummarySlice(n, ConstantGenerator)
 	for _, v := range vals {
 		assert.Equal(42.0, v)
 	}
-}
-func TestSummarySkiplistConstant10(t *testing.T) {
-	SummarySkiplistConstantN(t, 10)
-}
-func TestSummarySkiplistConstant100(t *testing.T) {
-	SummarySkiplistConstantN(t, 100)
-}
-func TestSummarySkiplistConstant1000(t *testing.T) {
-	SummarySkiplistConstantN(t, 1000)
-}
-func TestSummarySkiplistConstant10000(t *testing.T) {
-	SummarySkiplistConstantN(t, 10000)
-}
-func TestSummarySkiplistConstant100000(t *testing.T) {
-	SummarySkiplistConstantN(t, 100000)
 }
 func TestSummarySliceConstant10(t *testing.T) {
 	SummarySliceConstantN(t, 10)
@@ -105,23 +67,6 @@ func TestSummarySliceConstant100000(t *testing.T) {
 func UniformGenerator(i int) float64 {
 	return float64(i)
 }
-func SummarySkiplistUniformN(t *testing.T, n int) {
-	assert := assert.New(t)
-	vals := GenSummarySkiplist(n, UniformGenerator)
-
-	for i, v := range vals {
-		var exp float64
-		if testQuantiles[i] == 0 {
-			exp = 0
-		} else if testQuantiles[i] == 1 {
-			exp = float64(n) - 1
-		} else {
-			rank := math.Ceil(testQuantiles[i] * float64(n))
-			exp = rank - 1
-		}
-		assert.InDelta(exp, v, EPSILON*float64(n), "quantile %f failed, exp: %f, val: %f", testQuantiles[i], exp, v)
-	}
-}
 func SummarySliceUniformN(t *testing.T, n int) {
 	assert := assert.New(t)
 	vals := GenSummarySlice(n, UniformGenerator)
@@ -139,21 +84,6 @@ func SummarySliceUniformN(t *testing.T, n int) {
 		assert.InDelta(exp, v, EPSILON*float64(n), "quantile %f failed, exp: %f, val: %f", testQuantiles[i], exp, v)
 	}
 }
-func TestSummarySkiplistUniform10(t *testing.T) {
-	SummarySkiplistUniformN(t, 10)
-}
-func TestSummarySkiplistUniform100(t *testing.T) {
-	SummarySkiplistUniformN(t, 100)
-}
-func TestSummarySkiplistUniform1000(t *testing.T) {
-	SummarySkiplistUniformN(t, 1000)
-}
-func TestSummarySkiplistUniform10000(t *testing.T) {
-	SummarySkiplistUniformN(t, 10000)
-}
-func TestSummarySkiplistUniform100000(t *testing.T) {
-	SummarySkiplistUniformN(t, 100000)
-}
 func TestSummarySliceUniform10(t *testing.T) {
 	SummarySliceUniformN(t, 10)
 }
@@ -168,57 +98,6 @@ func TestSummarySliceUniform10000(t *testing.T) {
 }
 func TestSummarySliceUniform100000(t *testing.T) {
 	SummarySliceUniformN(t, 100000)
-}
-
-func NewSummaryWithTestData() *Summary {
-	s := NewSummary()
-
-	for i := 0; i < 1000; i++ {
-		s.Insert(float64(i), uint64(i))
-	}
-
-	return s
-}
-
-func TestSummaryGob(t *testing.T) {
-	assert := assert.New(t)
-
-	s := NewSummaryWithTestData()
-	bytes, err := s.GobEncode()
-	assert.Nil(err)
-	ss := NewSummary()
-	ss.GobDecode(bytes)
-
-	assert.Equal(s.N, ss.N)
-}
-
-func TestSummaryMerge(t *testing.T) {
-	assert := assert.New(t)
-	s1 := NewSummary()
-	for i := 0; i < 101; i++ {
-		s1.Insert(float64(i), uint64(i))
-	}
-
-	s2 := NewSummary()
-	for i := 0; i < 50; i++ {
-		s2.Insert(float64(i), uint64(i))
-	}
-
-	s1.Merge(s2)
-
-	expected := map[float64]float64{
-		0.0: 0,
-		0.2: 15,
-		0.4: 30,
-		0.6: 45,
-		0.8: 70,
-		1.0: 100,
-	}
-
-	for q, e := range expected {
-		v := s1.Quantile(q)
-		assert.Equal(e, v)
-	}
 }
 
 func TestSummarySliceMerge(t *testing.T) {
@@ -250,27 +129,6 @@ func TestSummarySliceMerge(t *testing.T) {
 	}
 }
 
-func TestSummaryRemergeReal10000(t *testing.T) {
-	s := NewSummary()
-	for n := 0; n < 1000; n++ {
-		s1 := NewSummary()
-		for i := 0; i < 100; i++ {
-			s1.Insert(float64(i), uint64(i))
-		}
-		s.Merge(s1)
-
-	}
-
-	fmt.Println(s)
-	slices := s.BySlices()
-	fmt.Println(slices)
-	total := 0
-	for _, s := range slices {
-		total += s.Weight
-	}
-	fmt.Println(total)
-}
-
 func TestSliceSummaryRemergeReal10000(t *testing.T) {
 	s := NewSliceSummary()
 	for n := 0; n < 10000; n++ {
@@ -300,25 +158,6 @@ func TestSliceSummaryRemerge10000(t *testing.T) {
 		}
 
 		//      fmt.Println(s1)
-	}
-
-	fmt.Println(s1)
-	slices := s1.BySlices()
-	fmt.Println(slices)
-	total := 0
-	for _, s := range slices {
-		total += s.Weight
-	}
-	fmt.Println(total)
-}
-
-func TestSummaryRemerge10000(t *testing.T) {
-	s1 := NewSummary()
-	for n := 0; n < 1000; n++ {
-		for i := 0; i < 100; i++ {
-			s1.Insert(float64(i), uint64(i))
-		}
-		//  fmt.Println(s1)
 	}
 
 	fmt.Println(s1)
